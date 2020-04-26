b8dcbe8e3e34b811ff8a2a8f95148151f3f4524b
Alexander Reinhardt <legendofmiracles@protonmail.com>
2020-04-26 10:56:21 +0200

    firefox:

--- firefox/db.js
+++ firefox/db.js
@@ -1,6 +1,26 @@
-var jsonFile
+var jsonFile;
 
-function fetchDB(){
-    
-    console.log(JSON.stringify(jsonFile))
+function fetchDB() {
+  // fetches the json database with the themes stored in it
+    fetch("themes.json")
+    .then(function (response) {
+      return response.json();
+    }).then(function (data) {
+        // Then processes the returned data in another function
+        appendData(data);
+      })
+    .catch(function (err) {
+      console.log(err);
+    });
 }
+
+function appendData(data) {
+    
+    var mainContainer = document.getElementById("myData");
+    // For the lenght 
+    for (var i = 0; i < data.themes.length; i++) {
+      var div = document.createElement("div");
+      div.innerHTML = 'Name: ' + data.themes[i].name;
+      mainContainer.appendChild(div);
+    }
+  }
